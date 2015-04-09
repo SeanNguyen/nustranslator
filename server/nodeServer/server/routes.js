166405955a664ede89562a94f476a7f118a2fb82
SeanNguyen <firestormkjc.1993@gmail.com>
2015-04-10 00:15:13 +0800

    change public url to view_record

--- server/nodeServer/server/routes.js
+++ server/nodeServer/server/routes.js
@@ -14,8 +14,8 @@ module.exports = function(app) {
   app.use('/api/sync/sentences', require('./api/sync/sentences'));
   app.use('/api/sync/dict', require('./api/sync/dict'));
   app.use('/api/sync/language_model', require('./api/sync/language_model'));
-  app.use('/public', express.static(__dirname + "/data/records"));
-  app.use('/public', serveIndex(__dirname + "/data/records"));
+  app.use('/view_record', express.static(__dirname + "/data/records"));
+  app.use('/view_record', serveIndex(__dirname + "/data/records"));
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
