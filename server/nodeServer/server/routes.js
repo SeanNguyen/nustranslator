64f50bf2cafb6c45d469a258db145e7ff397d3b1
SeanNguyen <firestormkjc.1993@gmail.com>
2015-04-10 00:06:09 +0800

    added browsing public files function

--- server/nodeServer/server/routes.js
+++ server/nodeServer/server/routes.js
@@ -7,12 +7,15 @@
 var errors = require('./components/errors');
 
 module.exports = function(app) {
-
+  var express = require('express');
+  var serveIndex = require('serve-index');
   // Insert routes below
   app.use('/api/update', require('./api/update'));
   app.use('/api/sync/sentences', require('./api/sync/sentences'));
   app.use('/api/sync/dict', require('./api/sync/dict'));
   app.use('/api/sync/language_model', require('./api/sync/language_model'));
+  app.use('/public', express.static(__dirname + "/data/records"));
+  app.use('/public', serveIndex(__dirname + "/data/records"));
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
