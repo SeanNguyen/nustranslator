062ae917987627e0356642dc318894ed37ebb17f
SeanNguyen <firestormkjc.1993@gmail.com>
2015-04-11 19:55:26 +0800

    server 0.1

--- server/nodeServer/server/app.js
+++ server/nodeServer/server/app.js
@@ -22,40 +22,43 @@ server.listen(config.port, config.ip, function () {
 });
 
 //init socket
-// var io = require('socket.io')(server);
-// io.on('connection', function(socket){
-// 	console.log('SOCKET: user connnect');
-// 	var filePath = './server/data/records/';
-// 	var fileExt = '';
-// 	var fs = require('fs');
-// 	var writeStream ;
+var io = require('socket.io')(server);
+io.on('connection', function(socket){
+	console.log('SOCKET: user connnect');
+	var filePath = './server/data/records/';
+	var fileExt = '';
+	var fs = require('fs');
+	var writeStream ;
+	var atob = require('atob')
 		
-// 	socket.on('disconnect', function(){
-// 		if (writeStream) {
-// 			writeStream.end();
-// 		}
-// 		console.log('SOCKET: user disconnect');
-// 	});
-// 	socket.on('dataType', function(message) {
-// 		console.log('SOCKET: data type: ' + message);
-// 		if (message == 'text') {
-// 			fileExt = '.txt';
-// 		} else if (message == 'audio') {
-// 			fileExt = '';
-// 		}
-// 	});
-// 	socket.on('fileName', function(message) {
-// 		console.log('SOCKET: file name: ' + message + fileExt);
-// 		writeStream = fs.createWriteStream(filePath + message + fileExt);
-// 	});
-// 	socket.on('data', function(message) {
-// 		console.log('SOCKET: data: ' + message);
-// 		writeStream.write(message);
-// 		if (fileExt == '.txt') {
-// 			writeStream.write('\n');
-// 		}
-// 	});
-// });
+	socket.on('disconnect', function(){
+		if (writeStream) {
+			writeStream.end();
+		}
+		console.log('SOCKET: user disconnect');
+	});
+	socket.on('dataType', function(message) {
+		console.log('SOCKET: data type: ' + message);
+		if (message == 'text') {
+			fileExt = '.txt';
+		} else if (message == 'audio') {
+			fileExt = '';
+		}
+	});
+	socket.on('fileName', function(message) {
+		console.log('SOCKET: file name: ' + message + fileExt);
+		writeStream = fs.createWriteStream(filePath + message + fileExt);
+	});
+	socket.on('data', function(message) {
+		if (fileExt == '.txt') {
+			writeStream.write(message);
+			writeStream.write('\n');
+		} else if (fileExt == '') {
+			message = atob(message);
+			writeStream.write(message);
+		}
+	});
+});
 
 // Expose app
 exports = module.exports = app;
